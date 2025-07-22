87a07ad557bd698072ceb2e46bdca70d49cf1297
tamslo <tamaraslosarek@gmail.com>
2025-07-22 20:10:19 +0200

    chore: lint ploidy script

--- data/scripts/adapt_y_ploidy.py
+++ data/scripts/adapt_y_ploidy.py
@@ -2,7 +2,7 @@
 
 import sys
 
-from vcf_utils import test_input_file, read_vcf, write_vcf, VCF_MISSING_VALUES
+from vcf_utils import test_input_file, read_vcf, write_vcf
 
 input_file_name = sys.argv[1]
 output_file_name = sys.argv[2]
@@ -12,9 +12,9 @@ test_input_file(input_file_name)
 input_data = read_vcf(input_file_name)
 
 for row_index, current in input_data.iterrows():
-    if current['chrom'] != 'Y':
+    if current["chrom"] != "Y":
         continue
-    current_genotype = current['sample']
-    if not '/' in current_genotype:
-        input_data.loc[row_index, 'sample'] = f'{current_genotype}/{current_genotype}'
+    current_genotype = current["sample"]
+    if "/" not in current_genotype:
+        input_data.loc[row_index, "sample"] = f"{current_genotype}/{current_genotype}"
 write_vcf(input_file_name, output_file_name, input_data)
